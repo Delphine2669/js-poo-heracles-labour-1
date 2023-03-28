76e2e7793c79761add00bc9cb17241b7164d4da5
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-03-28 12:21:31 +0200

    added code into both files, exercice hercules

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+// import { Fighter } from "./src/Fighter";
+// const { life } = require("./src/Fighter")
+const Fighter = require("./src/Fighter")
+
+const hero1 = new Fighter("👱🏻‍♂️ Heracles", 20, 6);
+const hero2 = new Fighter("🦁 Nemean Lion ", 11, 13);
+
+console.log(` ${hero1.name}, has ${hero1.max_Life} lives `)
+console.log(` ${hero2.name}, has ${hero2.max_Life} lives `)
+
+
+
+
+hero1.fight(hero2)
+hero2.fight(hero1)
+
+// console.log(`${hero1.name} ${hero1.fight(hero2)} ${hero2.name} ${hero2.max_Life}`)
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,21 @@
 /* Fighter class definition */
+
+const max_Life = 100
+
+class Fighter {
+    constructor(name, strength, dexterity, life = max_Life, fight) {
+        this.name = name
+        this.strength = strength
+        this.dexterity = dexterity
+        // this.max_Life = max_Life
+        this.fight = fight
+        function fight(defender) {
+            const dammagePoint = Math.floor(Math.random() * this.strength + 1);
+            const defenderDodge = Math.floor((dammagePoint - this.dexterity) > 0);
+            const lifeUpdate = (dammagePoint - defenderDodge);
+        }
+    }
+}
+
+
+module.exports = Fighter
